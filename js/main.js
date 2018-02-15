c3df34f3db74528f1d5fda4ab45aa820e26ca0c1
Warnock <b_warnock62905@tss-hr420-dm19.fc.ca>
2018-02-15 09:55:45 -0500

    Drumkit is a done deal

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// when the user presses a key, find the corresponding audio file on the page and play it
+//
+(() => {
+  console.log('drumkit js file loaded');
+
+function removeHighLight(e) {
+  //console.log(e);
+  if (e.propertyName !== "transform") {
+    return;
+  } else {
+      e.target.classList.remove('playing');
+    }
+
+  }  
+
+// run this whenever a key gets pressed
+function logKey(e) {
+  //debugger;
+  //console.log(e);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
+  // add a highlight to the key artwork on the screen
+  key.classList.add('playing');
+
+  // if there's no matching audio element , then don't do anything (avoid errors)
+  if (!audio) { return; }
+
+  // play the audio
+  audio.currentTime = 0
+  audio.play();
+
+}
+
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeHighLight));
+
+  window.addEventListener('keydown', logKey);
+})();
